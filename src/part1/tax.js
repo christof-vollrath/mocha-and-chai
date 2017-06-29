85098a8b51e99f8bfdf0abca90dd710935d0afbd
cvollrat <christof.vollrath@bild.de>
2017-06-29 14:27:55 +0200

    Test makes requests and are faked with nook

--- src/part1/tax.js
+++ src/part1/tax.js
@@ -1,6 +1,22 @@
 // src/part1/tax.js
+
+var request = require('request');
+
 module.exports = {
   calculate: function(subtotal, state, done) {
-    // implemented later or in parallel by our coworker
+      if (state !== 'CA') {
+          return done({ amount: 0 });
+      }
+
+      request.post({
+          url: 'https://some-tax-service.com/request',
+          method: 'POST',
+          json: {
+            subtotal: subtotal
+          }
+      }, function(error, response, body) {
+          done(body);
+      });
+
   }
 };
